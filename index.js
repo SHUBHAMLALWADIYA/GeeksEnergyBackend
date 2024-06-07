84cd229ac48c28b1daf19e677b1edced4504c765
unknown <lalwadiyashubham123@gmail.com>
2024-06-07 13:19:27 +0530

    all backend of geeksEnergy

---
+++ index.js
@@ -0,0 +1,36 @@
+const express = require("express");
+const axios = require("axios");
+const cors = require("cors");
+const app = express();
+const dotenv = require("dotenv");
+dotenv.config();
+const PORT = process.env.PORT||8080;
+
+
+
+app.use(cors());
+app.use(express.json());
+
+
+
+app.post("/api/movieList", async (req, res) => {
+  try {
+    const { category, language, genre, sort } = req.body;
+    const response = await axios.post("https://hoblist.com/api/movieList", {
+      category,
+      language,
+      genre,
+      sort,
+    });
+
+    res.json(response.data);
+  } catch (error) {
+    console.error("Error fetching data:", error);
+    res.status(500).json({ error: "Failed to fetch data" });
+  }
+});
+
+
+app.listen(PORT,()=>{
+    console.log(`server is Running at PORT ${PORT}`)
+})
